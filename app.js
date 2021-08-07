f13a44ae75c19a3044fe1b6f3016385634593f51
Swaraj Saxena <72065474+swarajsaxena@users.noreply.github.com>
2021-08-07 14:07:14 +0530

    few changes

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-const menuBtn = document.querySelector(".menu-btn");
-const navContainer = document.querySelector(".nav-container");
-
 // For roadtrip blog expand function
 const expand_roadtrip = document.querySelector(".expand_roadtrip");
 const roadtrip_desc = document.querySelector(".roadtrip-desc");
@@ -29,16 +26,19 @@ expand_digital.addEventListener("click", () => {
   }
 });
 
+const menuBtn = document.querySelector(".menu-btn");
+const navContainer = document.querySelector(".nav-container");
 let menuOpen = false;
 
 menuBtn.addEventListener("click", () => {
-  if (!menuOpen) {
-    menuBtn.classList.add("open");
-    menuOpen = true;
-    navContainer.classList.add("nav-open");
-  } else {
-    menuBtn.classList.remove("open");
-    menuOpen = false;
-    navContainer.classList.remove("nav-open");
-  }
+  // if (!menuOpen) {
+  //   menuBtn.classList.add("open");
+  //   menuOpen = true;
+  //   navContainer.classList.add("nav-open");
+  // } else {
+  //   menuBtn.classList.remove("open");
+  //   menuOpen = false;
+  //   navContainer.classList.remove("nav-open");
+  // }
+  console.log("hello");
 });
