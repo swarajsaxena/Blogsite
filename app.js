2ab727ba4ef7810d97ae4a8c205d61e24033a6c3
Swaraj Saxena <72065474+swarajsaxena@users.noreply.github.com>
2021-08-07 13:54:25 +0530

    responsive repair and blog page redefined

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
 const menuBtn = document.querySelector(".menu-btn");
 const navContainer = document.querySelector(".nav-container");
 
+// For roadtrip blog expand function
+const expand_roadtrip = document.querySelector(".expand_roadtrip");
+const roadtrip_desc = document.querySelector(".roadtrip-desc");
+
+expand_roadtrip.addEventListener("click", () => {
+  if (roadtrip_desc.classList.contains("desc-expanded")) {
+    roadtrip_desc.classList.remove("desc-expanded");
+    expand_roadtrip.innerHTML = "Expand"
+  } else {
+    roadtrip_desc.classList.add("desc-expanded");
+    expand_roadtrip.innerHTML = "Colapse"
+  }
+});
+
+// For digital blog expand function
+const expand_digital = document.querySelector(".expand_digital");
+const digital_desc = document.querySelector(".digital-desc");
+
+expand_digital.addEventListener("click", () => {
+  if (digital_desc.classList.contains("desc-expanded")) {
+    digital_desc.classList.remove("desc-expanded");
+    expand_digital.innerHTML = "Expand"
+  } else {
+    digital_desc.classList.add("desc-expanded");
+    expand_digital.innerHTML = "Colapse"
+  }
+});
+
 let menuOpen = false;
 
 menuBtn.addEventListener("click", () => {
